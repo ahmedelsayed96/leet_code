ea5648ce0af335a1389a527d1a9c5d36bd6b7074
Ahmed El-Sayed Ali <hatemido1122@gmail.com>
2022-05-29 02:33:06 +0200

    Time: 214 ms, Memory: 35.3 MB - LeetHub

---
+++ plus-one/plus-one.kt
@@ -0,0 +1,28 @@
+class Solution {
+    fun plusOne(digits: IntArray): IntArray {
+        var up =1;
+        var res =digits;
+        for (i in res.size-1 downTo 0){
+           up = res[i]+up
+            if(up  <10){
+                res[i]=up;
+                up =0;
+                break;
+            }else {
+                res[i]=up-10;
+                up=1;
+            }
+        }
+        if(up==1){
+        var newArr = IntArray(res.size+1);
+            newArr[0]=1;
+            for(i in 1 until res.size ){
+                newArr[i]=res[i-1];
+            }
+            res =newArr;
+        }
+        
+        return res;
+        
+    }
+}
