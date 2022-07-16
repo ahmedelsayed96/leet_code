847b6b398649f97a0fdee256b64b885e759e4ecd
Ahmed El-Sayed Ali <hatemido1122@gmail.com>
2022-07-16 18:21:50 +0200

    Time: 1 ms, Memory: 41.7 MB - LeetHub

---
+++ factorial-trailing-zeroes/factorial-trailing-zeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int trailingZeroes(int n) {
+        if(n==0) return 0;
+        
+        int count =0;
+       while(n>=5){
+           count+= n/5;
+           n=n/5;
+       }
+        
+         return count;
+        
+        
+    }
+   
+}
