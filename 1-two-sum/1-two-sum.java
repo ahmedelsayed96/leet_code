5ed9e98bbd8e23e846c6070f729ad041973ff3fc
Ahmed El-Sayed Ali <hatemido1122@gmail.com>
2022-08-28 22:03:35 +0200

    Time: 3 ms (92.53%), Space: 46 MB (22.21%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -3,18 +3,26 @@ class Solution {
        
         //[2,7,11,15], target = 9
         // On(n2)
-        
-        for(int i=0 ;i<nums.length;i++){
-            //3
-            for( int j=i+1;j<nums.length;j++){
-                //j =4
-                if(nums[i]+nums[j]==target)
-                    return new int[]{i,j};
-                
+        // 2 ,3 
+        Map<Integer,Integer> map  = new HashMap<Integer,Integer>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                return new int[]{map.get(nums[i]),i};
             }
-            
-            
+            map.put(target-nums[i],i);
         }
+        
+//         for(int i=0 ;i<nums.length;i++){
+//             //3
+//             for( int j=i+1;j<nums.length;j++){
+//                 //j =4
+//                 if(nums[i]+nums[j]==target)
+//                     return new int[]{i,j};
+                
+//             }
+            
+            
+//         }
         
         return new int[]{0,0};
     }
