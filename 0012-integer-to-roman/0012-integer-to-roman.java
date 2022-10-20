1a4e971fe448a38d83e6f5dd658e70bbe8293c33
Ahmed El-Sayed Ali <hatemido1122@gmail.com>
2022-10-20 23:58:56 +0200

    Time: 4 ms (99.95%), Space: 41.8 MB (96.33%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -4,33 +4,6 @@ class Solution {
     public String intToRoman(int num) {
         chars=new String[]{"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         values=new int[]{1000,900,500,400,100,90,50,40,10,9,5,4,1};  
-        StringBuilder  builder = new StringBuilder();
-        while(num>0){
-            int remain =0;
-            int count =1;
-            while(remain==0){
-
-                int  newNum = num/10;
-                int complete = newNum*10;
-                 remain =num-complete;
-                if(remain==0) {
-                    count*=10;
-                    num/=10;
-                }else{
-                    num*=count;
-                    num -=remain*count;
-
-                }
-            }
-            
-            remain *=count;
-            builder.insert(0,mapRemain(remain));
-
-        }
-       return builder.toString(); 
-    }
-    
-    public String mapRemain(int num){
         StringBuilder builder = new StringBuilder();
         
         for(int i=0;i<values.length;i++){
@@ -42,4 +15,6 @@ class Solution {
         }
        return builder.toString();
     }
+    
+
 }
